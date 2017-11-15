841413c285e61f79e90cc4eec799d12917ac18d6
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-15 20:29:49 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,6 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     rating = self.highest_rating
-    Show.where(":rating = #{rating}")
+    Show.where(":rating == #{rating}")
   end
 end
